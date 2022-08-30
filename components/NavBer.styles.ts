f7530eb249d43964cc8664c4a1f31e2d03b8b976
Songchangyeop <waitwait0301@naver.com>
2022-08-30 18:42:40 +0900

    next.js route practice

---
+++ components/NavBer.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+export const Nav = styled.nav`
+	display: flex;
+	justify-content: center;
+	color: tomato;
+`;
+
+export const NextLink = styled(Link)`
+	text-decoration: none;
+`;
